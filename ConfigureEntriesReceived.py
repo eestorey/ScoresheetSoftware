6cfa2224478a6a9b49b1d29803a701c9aed83596
Emily Storey <emily.e.storey@gmail.com>
2022-08-01 20:50:38 -0400

    forgot to put stuff in an env file

--- ConfigureEntriesReceived.py
+++ ConfigureEntriesReceived.py
@@ -1,11 +1,17 @@
+from dotenv import load_dotenv
+import os
 import pandas as pd
 
+load_dotenv()
+BCOEM_EXPORT_ENTRIES_FILE = os.getenv('BCOEM_EXPORT_ENTRIES_FILE')
+ENTRY_TRACKING_FILE = os.getenv('ENTRY_TRACKING_FILE')
+
 # Import a list of all entries to a pandas dataframe. 
-dfOld = pd.read_csv('Brew_Slam_2019_Entries_All_All_11-20-2019.csv', delimiter=',')
+dfOld = pd.read_csv(BCOEM_EXPORT_ENTRIES_FILE, delimiter=',')
 
 # Then make a new dataframe with columns: JudgingNumber, Received, Renamed.
 dfNew = dfOld[['Received', 'Judging Number']].copy()
 dfNew['Judging Number'] = dfNew['Judging Number'].str.upper()
 dfNew.insert(2, 'Renamed', 0)
 
-dfNew.to_csv('Entries_Received_Renamed.csv', index = False, sep = ',')
+dfNew.to_csv(ENTRY_TRACKING_FILE, index = False, sep = ',')
