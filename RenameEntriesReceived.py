79de22b8aeecc5ff9ab4a522d683b785f3709e26
Emily Storey <emily.e.storey@gmail.com>
2022-09-02 18:16:46 -0400

    makes relative working directory possible

--- RenameEntriesReceived.py
+++ RenameEntriesReceived.py
@@ -13,7 +13,6 @@ This is a program that I am using to replace the GTA Brews score
 sheet software that I built in matlab... 
 
 Stuff that is still to do... 
-1) write another file to deal with the ENTRY_TRACKING_FILE after all is completed. Look for things that got missed.
 4) write a README. 
 5) make a 'gemfile' https://stackoverflow.com/questions/19280249/python-equivalent-of-a-ruby-gem-file https://fuzzyblog.io/blog/python/2019/09/10/building-a-python-requirements-txt-file.html
 
@@ -37,6 +36,7 @@ load_dotenv()
 TESSERACT_OCR_PATH = os.getenv('TESSERACT_OCR_PATH')
 ENTRY_TRACKING_FILE = os.getenv('ENTRY_TRACKING_FILE')
 DIGITAL_EVALUATION_FILE = os.getenv('DIGITAL_EVALUATION_FILE')
+WORKING_DIRECTORY = os.getenv('WORKING_DIRECTORY')
 JUDGING_PDF_FILE = os.getenv('JUDGING_PDF_FILE')
 
 PDF_IMAGE_RESOLUTION = int(os.getenv('PDF_IMAGE_RESOLUTION'))
@@ -49,12 +49,11 @@ judge_pixel_location = tuple(np.array([0.2, 1.5, 3.6, 2.6]) * PDF_IMAGE_RESOLUTI
 # Declare tesseract OCR path.
 pytesseract.pytesseract.tesseract_cmd = TESSERACT_OCR_PATH
 
-df_entries = pd.read_csv(ENTRY_TRACKING_FILE, delimiter=',')
+df_entries = pd.read_csv(WORKING_DIRECTORY + '\\' + ENTRY_TRACKING_FILE, delimiter=',')
 df_entries['Pagenumber'] = df_entries['Pagenumber'].astype('object')
 
 # Convert the pdf into images for iterating
-working_directory = os.getcwd()
-pages = convert_from_path(working_directory + '\\' + JUDGING_PDF_FILE, PDF_IMAGE_RESOLUTION, grayscale=True)
+pages = convert_from_path(WORKING_DIRECTORY + '\\' + JUDGING_PDF_FILE, PDF_IMAGE_RESOLUTION, grayscale=True)
 
 # For each page in the PDF, perform OCR. Update the tracking file to match entry number with page number.
 for i in range(len(pages)):
@@ -86,14 +85,14 @@ for i in range(len(pages)):
 df_withpages = df_entries.dropna()
 
 # for each row in df_withpages, in a 'Results-Success' folder, write new pdfs of the pages indicated.
-inputpdf = PdfFileReader(open(working_directory + '\\' + JUDGING_PDF_FILE, "rb"))
+inputpdf = PdfFileReader(open(WORKING_DIRECTORY + '\\' + JUDGING_PDF_FILE, "rb"))
 
 # Look for missing page numbers. Write to a text file with the missing ones. 
 pgs_should_exist = set(range(inputpdf.numPages))
 pgs_got_matched = set(sum(df_withpages['Pagenumber'].tolist(), []))
 pgs_missing_matches = list(pgs_should_exist - pgs_got_matched)
 
-with open('pages_missing_matches.txt', 'w') as outfile:
+with open(WORKING_DIRECTORY + '\\' + 'pages_missing_matches.txt', 'w') as outfile:
     if pgs_missing_matches != []:
         outfile.writelines('Pages not matched to an entry\n')
         outfile.writelines((str(i+1)+'\n' for i in pgs_missing_matches))
@@ -102,21 +101,21 @@ with open('pages_missing_matches.txt', 'w') as outfile:
 
 # Source for pdf split https://stackoverflow.com/a/490203
 # Write each file that succeeded to a separate pdf in a new folder.
-Path(working_directory + '\\Results-Success\\').mkdir(parents=True, exist_ok=True)
+Path(WORKING_DIRECTORY + '\\Results-Success\\').mkdir(parents=True, exist_ok=True)
 for i in range(df_withpages.shape[0]) :
     row = df_withpages.iloc[i]
     output = PdfFileWriter()
     for j in range(len(row['Pagenumber'])) :
         output.addPage(inputpdf.getPage(row['Pagenumber'][j]))
-    with open(working_directory + '\\Results-Success\\' + row['Judging Number'] + '.pdf', 'wb') as outputStream:
+    with open(WORKING_DIRECTORY + '\\Results-Success\\' + row['Judging Number'] + '.pdf', 'wb') as outputStream:
         output.write(outputStream)
 
 # Write each page that failed to a separate pdf in a new folder.
-Path(working_directory + '\\Results-Fail\\').mkdir(parents=True, exist_ok=True)
+Path(WORKING_DIRECTORY + '\\Results-Fail\\').mkdir(parents=True, exist_ok=True)
 for i in pgs_missing_matches :
     output = PdfFileWriter()
     output.addPage(inputpdf.getPage(i))
-    with open(working_directory + '\\Results-Fail\\' + 'page-%s.pdf' % (i+1), 'wb') as outputStream:
+    with open(WORKING_DIRECTORY + '\\Results-Fail\\' + 'page-%s.pdf' % (i+1), 'wb') as outputStream:
         output.write(outputStream)
 
 
@@ -131,7 +130,7 @@ for i in pgs_missing_matches :
 # deal with the df_entries file. Flag entries that do not have exactly 2 pages found. 
 
 # Load the digital evaluations list. Needs to be cleaned up because HTML tables... 
-df_evaluations = pd.read_csv(DIGITAL_EVALUATION_FILE, delimiter=',').dropna(how = 'all').query('`Number` != "Number"').fillna(method = 'pad')
+df_evaluations = pd.read_csv(WORKING_DIRECTORY + '\\' + DIGITAL_EVALUATION_FILE, delimiter=',').dropna(how = 'all').query('`Number` != "Number"').fillna(method = 'pad')
 df_evaluations = df_evaluations[df_evaluations['Notes'].str.contains('Submitted|submitted')]
 
 # add a column for digital evaluations, tally up how many are already in the system. 
@@ -153,7 +152,7 @@ df_entries['Pages Found'] = df_entries['Pagenumber'].str.len()
 df_entries['Total Evaluations'] = df_entries['Pages Found'] + df_entries['Evaluations Submitted']
 
 # flag entries that don't have exactly 2 evaluations between digital and analog. 
-df_entries[df_entries['Total Evaluations'] != 2].to_csv('entries_without_2_evaluations.csv', index = False, sep = ',')
+df_entries[df_entries['Total Evaluations'] != 2].to_csv(WORKING_DIRECTORY + '\\entries_without_2_evaluations.csv', index = False, sep = ',')
 
-df_entries[['Received', 'Judging Number', 'Pagenumber', 'Evaluations Submitted', 'Pages Found', 'Total Evaluations']].to_csv(ENTRY_TRACKING_FILE, index = False, sep = ',')
+df_entries[['Received', 'Judging Number', 'Pagenumber', 'Evaluations Submitted', 'Pages Found', 'Total Evaluations']].to_csv(WORKING_DIRECTORY + '\\' + ENTRY_TRACKING_FILE, index = False, sep = ',')
         
